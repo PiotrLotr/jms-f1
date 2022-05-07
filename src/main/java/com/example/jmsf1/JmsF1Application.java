d323c768d1d21fa17ea23f54558395a8abb0e7a5
PiotrLotr <pwrona9@gmail.com>
2022-05-07 13:36:22 +0200

    v1.0.0 Configuring env for the app

---
+++ src/main/java/com/example/jmsf1/JmsF1Application.java
@@ -0,0 +1,13 @@
+package com.example.jmsf1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsF1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsF1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jmsf1/JmsF1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmsf1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsF1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
